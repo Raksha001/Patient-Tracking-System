b9436596fd7fba4347c59c60cf0179f066355813
Elizabeth Subhiksha <esubivictoria129@gmail.com>
2021-09-08 01:17:41 +0530

    added links

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-var serverUrl = "https://1e61-49-205-86-186.ngrok.io/";
+var serverUrl = "http://1e61-49-205-86-186.ngrok.io/";
--- js/view-profile.js
+++ js/view-profile.js
@@ -1,31 +1,27 @@
 // A $( document ).ready() block.
-$( document ).ready(function() {
-
-    $.ajax({
-        "url": serverUrl+"patientprofile",
-        "method": "POST",
-        "timeout": 0,
-        "headers": {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        "data": {
-            "uid": localStorage.uid,
-            "token": localStorage.token
-        }
-    }).done(function (response) {
-
-        if(response.status == "patient"){
-            document.getElementById("doctorName").innerHTML = response.doctorName;
-            document.getElementById("phone").innerHTML = response.phone;
-            document.getElementById("address").innerHTML = response.address;
-            document.getElementById("concern").innerHTML = response.concern;
-            document.getElementById("xray").innerHTML = response.xray;
-            document.getElementById("design").innerHTML = response.design;
-            document.getElementById("previousupload").innerHTML = response.previousupload;
-
-        } else {
-            window.location.replace("home.html");
-        }
-
-    });
+$(document).ready(function () {
+  $.ajax({
+    url:
+      serverUrl +
+      "patientprofile?uid=" +
+      localStorage.uid +
+      "&token=" +
+      localStorage.token,
+    method: "GET",
+    timeout: 0,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  }).done(function (response) {
+    response.patientDetails.forEach((patient)=>{
+        document.getElementById("doctorName").innerHTML = patient.doctorInCharge;
+        document.getElementById("phone").innerHTML = patient.phone;
+        document.getElementById("address").innerHTML = patient.address;
+        document.getElementById("concern").innerHTML = patient.concern;
+        document.getElementById("xray").innerHTML = patient.xray;
+        document.getElementById("design").innerHTML = patient.designFile;
+        document.getElementById("previousupload").innerHTML =
+          patient.previousupload;
+    })
+  });
 });
