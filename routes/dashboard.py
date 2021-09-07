87f94e41ab65b425c7094a2d6296eccc3a39d12f
veroni-d20 <67852686+veroni-d20@users.noreply.github.com>
2021-09-07 17:35:15 +0530

    register works without delete (#6)
    * logout works | register works without file upload
    
    * register and edit user works

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,6 +1,6 @@
 from app import app, forbidden, internal_server_error
 from db_config import mysql
-from flask import jsonify, requeste
+from flask import jsonify, request
 
 def sql_database(sql_query):
     try:
@@ -20,24 +20,38 @@ def dashboard():
     try:
         _token = request.json["token"]
         if _token:
-             sqlQuery = f"SELECT isAdmin FROM users WHERE uid='{_uid}' and '{_token}'"
-                isAdmin = sql_database(sqlQuery)
-                if isAdmin:
-                    if isAdmin[0] == 1:
-                        sqlQuery = f"SELECT count(uid) FROM patient_details"
-                        patientCount = sql_database(sqlQuery)
-                        sqlQuery = f"SELECT count(distinct(doctorInCharge)) FROM patient_details"
-                        doctorCount = sql_database(sqlQuery)
-                        sqlQuery = f"SELECT count(uid) FROM patient_details WHERE isCompleted=1"
-                        completedCount = sql_database(sqlQuery)
-                        sqlQuery = f"SELECT count(uid) FROM patient_details WHERE isCompleted=0"
-                        liveCount = sql_database(sqlQuery)
-                        return {'patientCount':patientCount,'doctorCount':doctorCount, 'completedCount':completedCount, 'liveCount':liveCount}
-                    else:
-                        return {'status':'Not admin'}
+            sqlQuery = f"SELECT isAdmin FROM users WHERE '{_token}'"
+            isAdmin = sql_database(sqlQuery)
+            if isAdmin:
+                if isAdmin[0] == 1:
+                    sqlQuery = f"SELECT count(uid) FROM patient_details"
+                    patientCount = sql_database(sqlQuery)
+                    sqlQuery = f"SELECT count(distinct(doctorInCharge)) FROM patient_details"
+                    doctorCount = sql_database(sqlQuery)
+                    sqlQuery = f"SELECT count(uid) FROM patient_details WHERE isCompleted=1"
+                    completedCount = sql_database(sqlQuery)
+                    sqlQuery = f"SELECT count(uid) FROM patient_details WHERE isCompleted=0"
+                    liveCount = sql_database(sqlQuery)
+                    sqlQuery = f"SELECT patientName, doctorInCharge, isCompleted, durationOfTreatment FROM patient_details"
+                    conn = mysql.connect()
+                    cursor = conn.cursor()
+                    cursor.execute(sql_query)
+                    row = cursor.fetchall()
+                    conn.commit()
+                    cursor.close()
+                    conn.close()
+                    if row:
+                        res = jsonify(row)
+                        return res
+                    return {'patientCount':patientCount,'doctorCount':doctorCount, 'completedCount':completedCount, 'liveCount':liveCount}
                 else:
-                    return {'status': 'null'}
+                    return {'status':'Not admin'}
+            else:
+                return {'status': null}
         else:
             return {'status': 'null token'}
     except Exception as e:
         return internal_server_error
+
+#file download
+#return redirect(url_for('download_file', name=filename))
