6390cd48ab41533712f3a375f40478b48d094dff
Raksha001 <vgraksha26@gmail.com>
2021-09-07 23:21:51 +0530

    Updates

--- js/registration.js
+++ js/registration.js
@@ -1,5 +1,15 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
+    var formdata = new FormData();
+    if (!!$('.uploadProfileInput').get(0).files[0])
+    formdata.append("xray", $('.uploadProfileInput').get(0).files[0], "ProfileImage." + $('.uploadProfileInput').get(0).files[0].name.split('.').pop());
+    formdata.append("firstName", document.getElementById('firstName').value);
+    formdata.append("lastName", document.getElementById('lastName').value);
+    formdata.append("address", document.getElementById('address').value);
+    formdata.append("phone", document.getElementById('phone').value);
+    formdata.append("concern", document.getElementById('concern').value);
+    formdata.append("duration", document.getElementById('duration').value);
+    formdata.append("startdate", document.getElementById('startdate').value);
 
     $.ajax({
         "url": "http://127.0.0.1:5000/register",
@@ -8,20 +18,11 @@ $( document ).ready(function() {
         "headers": {
             "Content-Type": "application/x-www-form-urlencoded"
         },
-        "data": {
-            "uid": localStorage.uid,
-            "token": localStorage.token
-        }
+        "data": formdata
     }).done(function (response) {
 
         if(response.status == "admin"){
-            document.getElementById("casesCompleted").innerHTML = response.casesCompleted;
-            document.getElementById("doctorCount").innerHTML = response.doctorCount;
-            document.getElementById("liveCount").innerHTML = response.liveCount;
-            document.getElementById("patientCount").innerHTML = response.patientCount;
-            document.getElementById("duration").innerHTML = response.duration;
-            document.getElementById("patientName").innerHTML = response.patientName;
-            document.getElementById("doctorName").innerHTML = response.doctorName;
+            window.location.replace("dashboard.html");
 
         } else {
             window.location.replace("error.html");
