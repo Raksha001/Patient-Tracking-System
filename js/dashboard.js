515bc1232cdb99b0600d4c50ccdfce25050edcb1
veroni-d20 <veronishwetha.23it@licet.ac.in>
2021-09-09 00:08:18 +0530

    dashboard.js changes

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,4 +1,3 @@
-// A $( document ).ready() block.
 $( document ).ready(function() {
 
     $.ajax({
@@ -9,19 +8,18 @@ $( document ).ready(function() {
             "Content-Type": "application/x-www-form-urlencoded"
         },
         "data": {
-            "uid": localStorage.uid,
             "token": localStorage.token
         }
     }).done(function (response) {
 
         if(response.status == "admin"){
-            document.getElementById("casesCompleted").innerHTML = response.casesCompleted;
+            document.getElementById("casesCompleted").innerHTML = response.completedCount;
             document.getElementById("doctorCount").innerHTML = response.doctorCount;
             document.getElementById("liveCount").innerHTML = response.liveCount;
             document.getElementById("patientCount").innerHTML = response.patientCount;
-            document.getElementById("duration").innerHTML = response.duration;
-            document.getElementById("patientName").innerHTML = response.patientName;
-            document.getElementById("doctorName").innerHTML = response.doctorName;
+            document.getElementById("duration").innerHTML = response.durationOfTreatment;
+            document.getElementById("patientName").innerHTML = response.users.patientName;
+            document.getElementById("doctorName").innerHTML = response.users.doctorInCharge;
 
         } else {
             window.location.replace("error.html");
